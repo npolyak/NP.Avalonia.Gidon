a4e7881e4a5b18be2ae990d501db0204428ba593
npolyak <nick.polyak@verizon.net>
2022-02-16 20:52:43 -0500

    checkpoint

--- Prototypes/GidonTest1/App.axaml.cs
+++ Prototypes/GidonTest1/App.axaml.cs
@@ -7,12 +7,12 @@ namespace GidonTest1
     public class App : Application
     {
         public override void Initialize()
-        {
+        {                
             AvaloniaXamlLoader.Load(this);
         }
 
         public override void OnFrameworkInitializationCompleted()
-        {
+        {  
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
                 desktop.MainWindow = new MainWindow();
