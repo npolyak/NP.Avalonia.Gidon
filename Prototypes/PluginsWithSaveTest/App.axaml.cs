1319f429babe2558e93107b4f953c8c0390bfe61
npolyak <nick.polyak@verizon.net>
2022-12-29 20:49:34 -0500

    switched over to enumerations

--- Prototypes/PluginsWithSaveTest/App.axaml.cs
+++ Prototypes/PluginsWithSaveTest/App.axaml.cs
@@ -4,6 +4,7 @@ using Avalonia.Markup.Xaml;
 using NP.Avalonia.Gidon;
 using NP.DependencyInjection.Interfaces;
 using NP.NLogAdapter;
+using System;
 
 namespace PluginsWithSaveTest
 {
@@ -15,15 +16,15 @@ namespace PluginsWithSaveTest
         /// "Plugins/Services" - to load the services (non-visual singletons)
         /// "Plugins/ViewModelPlugins" - to load view model plugins
         /// "Plugins/ViewPlugins" - to load view plugins
-        public static PluginManager ThePluginManager { get; } = 
-            new PluginManager
+        public static PluginManager<Enum> ThePluginManager { get; } = 
+            new PluginManager<Enum>
             (
                 "Plugins/Services", 
                 "Plugins/ViewModelPlugins", 
                 "Plugins/ViewPlugins");
 
         // the IoC container
-        public static IDependencyInjectionContainer TheContainer => ThePluginManager.TheContainer;
+        public static IDependencyInjectionContainer<Enum> TheContainer => ThePluginManager.TheContainer;
 
         public App()
         {
