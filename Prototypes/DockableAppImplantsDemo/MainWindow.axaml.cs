3b8dbc7b1f34281226d7b07bfb302f2dd1b27333
npolyak <nick.polyak@verizon.net>
2023-02-24 16:04:55 -0500

    simplified the main project

--- Prototypes/DockableAppImplantsDemo/MainWindow.axaml.cs
+++ Prototypes/DockableAppImplantsDemo/MainWindow.axaml.cs
@@ -10,48 +10,17 @@ namespace DockableAppImplantsDemo
 {
     public partial class MainWindow : Window
     {
-        IDisposable? _clientSubscription;
         public MainWindow()
         {
             InitializeComponent();
 //#if DEBUG
             this.AttachDevTools();
 //#endif
-
-            _clientSubscription =
-                App.TheRelayClient
-                   .ObserveTopicStream<WindowInfo>(Topic.WindowInfoTopic)
-                   .Subscribe(OnWindowInfoArrived);
         }
-
-
-        #region ChildWindowHandle Styled Avalonia Property
-        public nint ChildWindowHandle
-        {
-            get { return GetValue(ChildWindowHandleProperty); }
-            set { SetValue(ChildWindowHandleProperty, value); }
-        }
-
-        public static readonly StyledProperty<nint> ChildWindowHandleProperty =
-            AvaloniaProperty.Register<MainWindow, nint>
-            (
-                nameof(ChildWindowHandle)
-            );
-        #endregion ChildWindowHandle Styled Avalonia Property
-
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
-
-            //var handle = this.GetWinHandle();
-
-            //string currentDir = Directory.GetCurrentDirectory();
-        }
-
-        private void OnWindowInfoArrived(WindowInfo windowInfo)
-        {
-            ChildWindowHandle = (nint) windowInfo.WindowHandle;
         }
     }
 }
